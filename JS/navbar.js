332453d182a60f9eb62577038fdef2f35d759cec
Kunj Parikh <9002birdwatcher@gmail.com>
2024-10-04 23:00:02 -0400

    Donate button issue fix

--- JS/navbar.js
+++ JS/navbar.js
@@ -12,3 +12,10 @@ const imgLink = document.querySelector(".logoimg");
 imgLink.addEventListener('click', () => {
     window.location.href = "../HTML/index";
 });
+
+const navbarDonate = document.getElementById('navbar-donate');
+navbarDonate.addEventListener('click', () => {
+    window.open("https://secure.acsevents.org/site/STR?fr_id=108592&pg=team&team_id=2739088", '_blank').focus();
+    // navbarDonate.style.cursor = "url('pointer'), auto";
+    // window.location.href='https://secure.acsevents.org/site/STR?fr_id=108592&pg=team&team_id=2739088';
+});
